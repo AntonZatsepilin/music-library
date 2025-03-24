3ded3829b7be13a945ff77fe7bb1f8a74899bb41
AntonZatsepilin <zatsepilin90.00@bk.ru>
2025-03-24 17:37:02 +0300

    Fix formatting in server.go for consistency

--- backend/internal/models/server.go
+++ backend/internal/models/server.go
@@ -15,7 +15,7 @@ func (s *Server) Run(port string, handler http.Handler) error {
 		Addr:           ":" + port,
 		Handler:        handler,
 		MaxHeaderBytes: 1 << 20,
-		ReadTimeout:    10 * time.Second,
+		ReadTimeout:    10 * time.Second,  
 		WriteTimeout:   10 * time.Second,
 	}
 	return s.httpServer.ListenAndServe()
